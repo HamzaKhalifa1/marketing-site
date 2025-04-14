37ee0e0d030772094d0ece1b8b23e6c6e21990ce
HamzaKhalifa1 <enghamzakha@gmail.com>
2025-04-14 05:16:37 +0300

    Create the Footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import styles from './Footer.module.css'
+
 
 function Footer() {
     return (
-        <footer>
-                <h1>Footer</h1>
+        <footer id={styles.footer}>
+            <div className={styles.inFooter}>
+                <div className={`${styles.footerContain} ${styles.footerContainHeaderLogo}`}>Exclusive</div>
+                <div className={styles.footerContain}>Subscribe</div>
+                <div className={styles.footerContain}>Get 10% off your first order</div>
+                <div className={styles.footerContain}>Hi</div>
+            </div>
+            <div className={styles.inFooter}>
+                <div className={`${styles.footerContain} ${styles.footerContainHeader}`}>Support</div>
+                <div className={styles.footerContain}>111 Bijoy sarani, Dhaka,
+                    DH 1515, Banglade</div>
+                <div className={styles.footerContain}>exclusive@gmail.com</div>
+                <div className={styles.footerContain}>+88015-88888-9999</div>
+            </div>
+            <div className={styles.inFooter}>
+                <div className={`${styles.footerContain} ${styles.footerContainHeader}`}>Account</div>
+                <div className={styles.footerContain}>Shop</div>
+                <div className={styles.footerContain}>cost</div>
+                <div className={styles.footerContain}>Wishlist</div>
+            </div>
+            <div className={styles.inFooter}>
+                <div className={`${styles.footerContain} ${styles.footerContainHeader}`}>Quick Link</div>
+                <div className={styles.footerContain}>Privacy policy</div>
+                <div className={styles.footerContain}>Terms Of Use</div>
+                <div className={styles.footerContain}>Contact</div>
+            </div>
         </footer>
     );
 }
