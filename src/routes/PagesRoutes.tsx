742a9e8521189be34b6e5b1f350631c5ffe030fa
HamzaKhalifa1 <enghamzakha@gmail.com>
2025-04-16 06:40:44 +0300

    Implemented form section on CheckOut page

--- src/routes/PagesRoutes.tsx
+++ src/routes/PagesRoutes.tsx
@@ -3,16 +3,19 @@ import Layout from "../pages/Layout";
 import Home from "../pages/Home";
 import React from "react";
 import Wishlist from "../pages/Wishlist";
+import Cart from "../pages/Cart";
+import CheckOut from "../pages/CheckOut";
 
 const PagesRoutes=()=>{
     const route = createBrowserRouter([
         {path:'/',element:<Layout/>,
             children:[
                 {index:true,element:<Home/>},
-                {path:'Contact',element:<div>Contact</div>},
-                {path:'About',element:<div>About</div>},
-                {path:'cart',element:<div>Cart</div>},
+                {path:'contact',element:<div>Contact</div>},
+                {path:'about',element:<div>About</div>},
                 {path:'wishlist',element:<Wishlist/>},
+                {path:'cart',element:<Cart/>},
+                {path:'cart/CheckOut',element:<CheckOut/>},
             ]}
     ])
 
